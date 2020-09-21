1cd12ba1745316c890005e047c39b325031839e9
Rimsha98 <71652125+Rimsha98@users.noreply.github.com>
2020-09-21 19:56:58 +0500

    Add files via upload

---
+++ Classes/Utilities1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace UokSemesterSystem.Classes
+{
+    public static class Utilities1
+    {  public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["ConString"].ConnectionString.ToString();
+        }
+    }
+}
